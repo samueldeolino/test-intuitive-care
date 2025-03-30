a4dbd83f7882228ba106dd159251d206c7932373
Samuel Deolino <samueldeolino@alu.ufc.br>
2025-03-30 19:51:46 -0300

    readPdf function

--- data-transformation/transformation.py
+++ data-transformation/transformation.py
@@ -1,16 +1,14 @@
+from typing import List
 import tabula
 import pandas as df
 import os
 import zipfile as zip
 from pathlib import Path
+import shutil
 
 DIR = Path(__file__).parent.parent
 FOLDER_PATH = DIR /'web-scraping'/'scripts'/'Attachments_ANS.zip'
-
-print(FOLDER_PATH)
-OD = 'Seg. Odontológica'.upper()
-AMB = 'Seg. Ambulatorial'.upper()
-columns_map = {'OD': OD, 'AMB': AMB}
+PDF_NAME = 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf'
 
 def unzipFolder(zipPath: str, pdfName: str):
 
@@ -21,4 +19,9 @@ def unzipFolder(zipPath: str, pdfName: str):
             raise ValueError('File not found in zip')
         file.extract(pdfName, '.')
 
-unzipFolder(FOLDER_PATH, 'Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf')
+def readPdf():
+    try:
+        dfs = tabula.read_pdf(PDF_NAME, pages='all', multiple_tables=True, lattice=True)
+        return dfs
+    except Exception as e:
+        raise RuntimeError('Failure to reader PDF: {e}')
